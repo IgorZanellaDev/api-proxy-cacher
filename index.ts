a99423e524c57e516b5189024914aaf0de12fbdb
Igor Zanella <igorzanella.dev@gmail.com>
2023-12-18 01:14:27 +0100

    feat: add query caching

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import axios from "axios";
 import dotenv from "dotenv";
 import * as https from "https";
 import fastifyCors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
 const server = fastify();
+const prisma = new PrismaClient();
 
 const main = async (request: FastifyRequest, reply: FastifyReply) => {
   const { body, headers, method, query, url } = request;
@@ -27,7 +29,7 @@ const main = async (request: FastifyRequest, reply: FastifyReply) => {
     params: query,
     httpsAgent: agent,
   })
-    .then((response) => {
+    .then(async (response) => {
       const parsedHeaders = Object.entries(response.headers).reduce(
         (acc, [key, value]) => ({
           ...acc,
@@ -35,6 +37,46 @@ const main = async (request: FastifyRequest, reply: FastifyReply) => {
         }),
         {},
       );
+
+      const cachedRequest = await prisma.request.findFirst({
+        orderBy: [
+          {
+            updated_at: "desc",
+          },
+        ],
+        where: {
+          method,
+          request_url: targetUrl,
+          request_payload: JSON.stringify(body),
+        },
+      });
+
+      if (cachedRequest) {
+        await prisma.request.update({
+          where: {
+            id: cachedRequest.id,
+          },
+          data: {
+            request_headers: JSON.stringify(headers),
+            response_status: response.status,
+            response_body: JSON.stringify(response.data),
+            response_headers: JSON.stringify(parsedHeaders),
+          },
+        });
+      } else {
+        await prisma.request.create({
+          data: {
+            method,
+            request_url: targetUrl,
+            request_headers: JSON.stringify(headers),
+            request_payload: JSON.stringify(body),
+            response_status: response.status,
+            response_body: JSON.stringify(response.data),
+            response_headers: JSON.stringify(parsedHeaders),
+            updated_at: new Date(),
+          },
+        });
+      }
 
       reply.headers({ ...parsedHeaders });
       reply.status(response.status);
